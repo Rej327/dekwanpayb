8cd70da9da8b5382c6be4f0c6c51df30f6adf47d
Rej327 <resujeff27@gmail.com>
2025-12-15 15:53:17 +0800

    feat: additional setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-// page.tsx
-import Link from 'next/link'
+'use client'
+
+import { useState } from 'react'
+import { MainShell } from './components/MainShell'
+import { Dashboard } from './components/Dashboard'
+import { TaskManager } from './components/TaskManager'
+import { Settings } from './components/Settings'
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState('Dashboard')
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case 'Dashboard':
+        return <Dashboard />
+      case 'Tasks':
+        return <TaskManager />
+      case 'Settings':
+        return <Settings />
+      default:
+        return <Dashboard />
+    }
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
+    <MainShell activeTab={activeTab} onTabChange={setActiveTab}>
+      {renderContent()}
+    </MainShell>
   )
 }
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,25 @@
 // jest.setup.ts
 // note past breaking change: https://stackoverflow.com/questions/77328459/cannot-find-module-testing-library-jest-dom-extend-expect-from-jest-setup-js
 import '@testing-library/jest-dom'
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
+
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.ResizeObserver = ResizeObserver
